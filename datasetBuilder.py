59b66bbf56142b9329d4e4dc3765b276b8711dac
schuywalker <schuylerasplin@gmail.com>
2023-05-04 16:06:48 -0700

    set up main

---
+++ datasetBuilder.py
@@ -0,0 +1,9 @@
+
+class DatasetBuilder():
+    
+    def __init__(self, ticker) -> None:
+        self.ticker = ticker
+    
+    def getChain(self):
+        print(self.ticker)
+        
---
+++ main.py
@@ -0,0 +1,8 @@
+from datasetBuilder import *
+
+
+
+if __name__ == '__main__':
+    dsb = DatasetBuilder('MSFT')
+    dsb.getChain()
+    
