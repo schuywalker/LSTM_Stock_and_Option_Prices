db4f8ed3c5dea89b6bea60a13b89a40fc053009b
schuyler <sasplin@ewu.edu>
2023-05-17 22:38:00 -0700

    delete main, run datasetBuilder. string in file works better than command line args for complex column names

--- datasetBuilder.py
+++ datasetBuilder.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+import sys
 
 class DatasetBuilder():
     
@@ -14,8 +15,22 @@ class DatasetBuilder():
             newDataFrame[column] = srcDataFrame[column]
         newDataFrame.to_csv(destName, index=False, header=True, encoding='utf-8')
     
+if __name__ == '__main__':
+    dsb = DatasetBuilder('tsla_2019_2022.csv', 'tslaReduced.csv',' [UNDERLYING_LAST]', ' [EXPIRE_DATE]',
+       ' [EXPIRE_UNIX]', ' [DTE]', ' [C_DELTA]')
         
-
+'''
+headers in kaggle tsla_2019_2022.csv. have to add single quotes like 'col' or usually ' col'
+Index(['[QUOTE_UNIXTIME]', ' [QUOTE_READTIME]', ' [QUOTE_DATE]',
+       ' [QUOTE_TIME_HOURS]', ' [UNDERLYING_LAST]', ' [EXPIRE_DATE]',
+       ' [EXPIRE_UNIX]', ' [DTE]', ' [C_DELTA]', ' [C_GAMMA]', ' [C_VEGA]',
+       ' [C_THETA]', ' [C_RHO]', ' [C_IV]', ' [C_VOLUME]', ' [C_LAST]',
+       ' [C_SIZE]', ' [C_BID]', ' [C_ASK]', ' [STRIKE]', ' [P_BID]',
+       ' [P_ASK]', ' [P_SIZE]', ' [P_LAST]', ' [P_DELTA]', ' [P_GAMMA]',
+       ' [P_VEGA]', ' [P_THETA]', ' [P_RHO]', ' [P_IV]', ' [P_VOLUME]',
+       ' [STRIKE_DISTANCE]', ' [STRIKE_DISTANCE_PCT]'],
+'''
+        
 
 
         
--- main.py
+++
@@ -1,8 +0,0 @@
-from datasetBuilder import *
-
-
-
-if __name__ == '__main__':
-    dsb = DatasetBuilder('tsla_2019_2022.csv', 'tslaReduced.csv',' [UNDERLYING_LAST]',' [DTE]')
-
-    
