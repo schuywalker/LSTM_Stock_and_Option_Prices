6b8b0066fc79d85cdc06406d21d8f8e4f6fb7f23
schuyler <sasplin@ewu.edu>
2023-06-09 14:20:07 -0700

    add lstm output images

--- README.md
+++ README.md
@@ -1,18 +1,17 @@
 # Leveraging LSTM Stock Price Predictions With Options
 
-Authors: Schuyler Asplin, Vlad Shingarey, Parker Groop 
+Authors: Schuyler Asplin, Vlad Shingarey, Parker Groop
 
 This project is broken into 2 main parts:
-1) Using Long Short Term Memory to attempt stock price prediction
-2) Attempting to find a buy-side, option-based strategy that achieves better average returns than using LSTM to bet on the shares (or similar returns with reduced risk).
+
+1. Using Long Short Term Memory to attempt stock price prediction
+2. Attempting to find a buy-side, option-based strategy that achieves better average returns than using LSTM to bet on the shares (or similar returns with reduced risk).
 
 # Part 1: Predicting Price
 
 Result of our price predictions are below in orange, with the actual price in blue:
 
-
-
-
+![Screenshot](screenshots/LSTM_outputs/AAPL_price_prediction.png)
+![Screenshot](screenshots/LSTM_outputs/AAPL_error_over_time.png)
 
 # Part 2: Leveraging Predictions With Options
-
--- utils/datasetBuilder.py
+++ utils/datasetBuilder.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 class DatasetBuilder():
+
+    # stand alone script to remove columns from csv files and save as a new csv for faster load times.
     
     def __init__(self, srcName:str, destName:str, *desiredColumns) -> None:
         srcDataFrame = pd.read_csv(srcName)
@@ -18,21 +20,9 @@ class DatasetBuilder():
 #        ' [DTE]', ' [C_LAST]',
 #         ' [C_BID]', ' [C_ASK]', ' [STRIKE]', ' [P_BID]',
 #        ' [P_ASK]', ' [P_LAST]',)
-        
-'''
-headers in kaggle tsla_2019_2022.csv. have to add single quotes like 'col' or usually ' col'
-Index(['[QUOTE_UNIXTIME]', ' [QUOTE_READTIME]', ' [QUOTE_DATE]',
-       ' [QUOTE_TIME_HOURS]', ' [UNDERLYING_LAST]', ' [EXPIRE_DATE]',
-       ' [EXPIRE_UNIX]', ' [DTE]', ' [C_DELTA]', ' [C_GAMMA]', ' [C_VEGA]',
-       ' [C_THETA]', ' [C_RHO]', ' [C_IV]', ' [C_VOLUME]', ' [C_LAST]',
-       ' [C_SIZE]', ' [C_BID]', ' [C_ASK]', ' [STRIKE]', ' [P_BID]',
-       ' [P_ASK]', ' [P_SIZE]', ' [P_LAST]', ' [P_DELTA]', ' [P_GAMMA]',
-       ' [P_VEGA]', ' [P_THETA]', ' [P_RHO]', ' [P_IV]', ' [P_VOLUME]',
-       ' [STRIKE_DISTANCE]', ' [STRIKE_DISTANCE_PCT]'],
-'''
 
 if __name__ == '__main__':
-    dsb = DatasetBuilder('./apple/21savage.txt', 'AAPL_chains_reduced_21_22Jan.csv',' [QUOTE_DATE]',' [UNDERLYING_LAST]', ' [EXPIRE_DATE]',
+    dsb = DatasetBuilder('./apple/21_master_list.txt', 'AAPL_chains_reduced_21_22Jan.csv',' [QUOTE_DATE]',' [UNDERLYING_LAST]', ' [EXPIRE_DATE]',
         ' [DTE]', ' [C_LAST]',
             ' [C_BID]', ' [C_ASK]', ' [STRIKE]', ' [P_BID]',
         ' [P_ASK]', ' [P_LAST]',)
